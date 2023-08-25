d8f57133d53b86a5298f2afc6e7c1911e7f648ef
Melad Karmy <meladkarmy6@gmail.com>
2023-08-25 19:20:15 +0300

    update lang

--- DashBoard/src/app/app.component.ts
+++ DashBoard/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'DashBoard';
+  textDir:any='ltr'
   constructor(private translate:TranslateService){  }
 langue:boolean=false
   translater()
@@ -15,15 +16,32 @@ langue:boolean=false
     if(this.langue==false){
       this.translate.use('ar')
       document.dir = 'rtl';
+      console.log(this.translate)
     } 
     else {
        this.translate.use('en')
        document.dir = 'ltr';
+       console.log(this.translate)
+       this.fun()
+
     }
   }
   transformTo()
   {
     this.langue=!this.langue
     this.translater()
+    this.fun()
+  }
+  fun(){
+    this.translate.onLangChange.subscribe((event: LangChangeEvent)=>{
+      if(event.lang == 'ar')
+      {
+        this.textDir = 'rtl';
+      } 
+      else
+      {
+        this.textDir = 'ltr';
+      }
+    })
   }
 }
